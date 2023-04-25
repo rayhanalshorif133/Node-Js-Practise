dcbcbfee3ae4057263475c1bfcc64dedc59bc155
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-04-25 16:15:14 +0600

    write

--- index.js
+++ index.js
@@ -3,77 +3,13 @@ const http = require('http');
 const inputData = require('./inputData');
 
 
-const ourReadStream = 
-fs.createReadStream(`${__dirname}/bigData.txt`, 'utf8');
+const ourReadStream =
+    fs.createReadStream(`${__dirname}/bigData.txt`);
 
-// ourReadStream.on('data', (data) => {
-//     console.log(data);
-// });
+const ourWriteStream =
+    fs.createWriteStream(`${__dirname}/output.txt`);
 
-const server = new http.createServer((req, res) => {
-    if(req.url === '/') {
-        let html = `
-        <html>
-            <head>
-                <title>NodeJS</title>
-            </head>
-            <body>
-                <h1>NodeJS</h1>
-                <p>Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine.</p>
-            
-                <form method="POST" action="/process">
-                    <textarea id="w3review" name="w3review" rows="4" cols="50">
-                    ${inputData}
-                    </textarea>
-                    <br />
-                    <input type="submit" value="Submit" />
-                </form>
 
-                </body>
-        </html>
-        `;
-        res.write(html);
-        res.end();
-    } else if (req.url === '/process' && req.method === 'POST') {
-        let html = "";
-        // req.on('data', (data) => {
-        //     let textData = data.toString();
-        //     let len = textData.length;
-
-        //     html += `<p>Data length: : ${len}</p>`;
-
-        // html += `
-        // <html>
-        //     <head>
-        //         <title>NodeJS</title>
-        //     </head>
-        //     <body>
-
-        //         <p>${textData}</p>
-        //         </body>
-        // </html>
-        // `;
-        let body = [];
-        req.on('data', (data) => {
-            body.push(data);
-        });
-        req.on('end', () =>{
-            console.log("end");
-            var main_body = Buffer.concat(body).toString();
-            console.log(main_body);
-        });
-        res.write("Thank you for submitting the form");
-        res.end();
-    } else {
-        res.write('404');
-        res.end();
-    }
-
+ourReadStream.on('data', (chunk) => {
+    ourWriteStream.write(chunk);
 });
-
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
-
-
-// Path: bigData.txt
