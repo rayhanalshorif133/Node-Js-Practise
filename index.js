74c90fe6d20843551b245bdd94f443499dcc1e78
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-04-25 14:37:49 +0600

    emitter

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 // Register a listener for bellRing event
-emitter.on('bellRing', () => {
-    console.log('Making a noise, emit an event');
+emitter.on('bellRing', (period) => {
+    console.log(`We need to run because ${period}`);
 });
 
-emitter.emit('bellRing'); // Making a noise, emit an event
+emitter.emit('bellRing','2nd period ended'); // Making a noise, emit an event
 
