2a74e1f5a0968fc9d00cbdd52d88eee14e754f9b
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-04-25 14:39:37 +0600

    object pass

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 // Register a listener for bellRing event
-emitter.on('bellRing', (period) => {
-    console.log(`We need to run because ${period}`);
+emitter.on('bellRing', ({period,text}) => {
+    console.log(`We need to run because ${period} ${text}`);
 });
 
-emitter.emit('bellRing','2nd period ended'); // Making a noise, emit an event
+emitter.emit('bellRing',{
+    period: '2nd',
+    text: 'period ended'
+}); // Making a noise, emit an event
 
